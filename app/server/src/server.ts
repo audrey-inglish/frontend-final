9f8737369c9dcc4702577545129ef1adfa1995fc
Audrey Inglish <audreyinglish@gmail.com>
2025-11-22 12:51:03 -0700

    Increase file size limit

--- app/server/src/server.ts
+++ app/server/src/server.ts
@@ -17,7 +17,8 @@ import extractTextFromImageRouter from "./routes/extractTextFromImage";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.get("/api/hello", (_req, res) => {
   res.json({ message: "Hello from backend!" });
