2ea182e5356d819b37dde92be0b54dc7abd6199e
Audrey Inglish <audreyinglish@gmail.com>
2025-12-05 20:42:28 -0700

    Remove unused file

--- app/client/src/components/flashcard/index.ts
+++ app/client/src/components/flashcard/index.ts
@@ -1,5 +1,4 @@
 export { FlipCard } from "./FlipCard";
-export { FlashcardActionButtons } from "./FlashcardActionButtons";
 export { FlashcardPageHeader } from "./FlashcardPageHeader";
 export { FlashcardEmptyState } from "./FlashcardEmptyState";
 export { FlashcardFilter } from "./FlashcardFilter";
